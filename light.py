c6a4a50bc222867cdf5916a4d2667b2aa025f02e
lj <thinkpad>
2017-12-03 15:30:32 +0800

    Initial commit

---
+++ light.py
@@ -0,0 +1,14 @@
+import httplib
+import time
+conn =httplib.HTTPConnection("192.168.1.13",80)
+def on():
+    conn.request("GET", url="/on")
+    print conn.getresponse().read()
+
+def off():
+    conn.request("GET", url="/off")
+    print conn.getresponse().read()
+
+on()
+time.sleep(25)
+#off()
